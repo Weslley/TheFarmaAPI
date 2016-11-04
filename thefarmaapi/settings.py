568d0fcee25f47ee685dd750a95fc4dba6332e05
Lucas Cardoso <mr.lucascardoso@gmail.com>
2016-11-04 00:34:20 -0300

    DEBUG TRUE

--- thefarmaapi/settings.py
+++ thefarmaapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1fw$lp)2r2askby^#=-!$i36l_8frl46k(-%jmv%1cx-!4rlk='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
