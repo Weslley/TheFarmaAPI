0ba025546da9da00bbfbfa1887e81276989f2a8d
Lucas Cardoso <mr.lucascardoso@gmail.com>
2016-12-14 12:49:05 -0300

    Institui??o criada

---
+++ api/models/instituicao.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Instituicao(models.Model):
+    razao_social = models.CharField(max_length=120)
+    nome_fantasia = models.CharField(max_length=70)
+    cpf_cnpj = models.CharField(verbose_name='CPF/CNPJ', max_length=14)
+    site = models.URLField()
+    logo = models.ImageField(upload_to='instituicoes')
+    telefone = models.CharField(max_length=11)
+    data_criacao = models.DateTimeField(verbose_name='Data de criação', auto_now_add=True)
+    data_atualizacao = models.DateTimeField(verbose_name='Data de atualização', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Instituição'
+
+    def __str__(self):
+        return self.nome_fantasia
+
+
+class UsuarioInstituicao(models.Model):
+    instituicao = models.ForeignKey(Instituicao, related_name='usuarios')
+    user = models.ForeignKey(User, related_name='instituicao')
+
+    class Meta:
+        unique_together = ('instituicao', 'user')
