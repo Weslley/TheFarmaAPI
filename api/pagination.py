70029f4f70aebd01ff9567c3e3dda227ce5f143e
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-02-08 11:33:19 -0300

    Exibindo

--- api/pagination.py
+++ api/pagination.py
@@ -1,25 +1,52 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, _positive_int
+from rest_framework.response import Response
+from collections import OrderedDict
 
 
-class ExtraLargeResultsSetPagination(PageNumberPagination):
+class CustomPageNumberPagination(PageNumberPagination):
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('num_pages', self.page.paginator.num_pages),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))
+
+    def get_page_size(self, request):
+        if self.page_size_query_param:
+            try:
+                return _positive_int(
+                    request.query_params[self.page_size_query_param],
+                    strict=True,
+                    cutoff=self.max_page_size
+                )
+            except (KeyError, ValueError):
+                pass
+
+        return self.page_size
+
+
+class ExtraLargeResultsSetPagination(CustomPageNumberPagination):
     page_size = 10000
     page_size_query_param = 'page_size'
-    max_page_size = 100000
+    max_page_size = 1000000
 
 
-class LargeResultsSetPagination(PageNumberPagination):
+class LargeResultsSetPagination(CustomPageNumberPagination):
     page_size = 5000
     page_size_query_param = 'page_size'
-    max_page_size = 50000
+    max_page_size = 1000000
 
 
-class StandardResultsSetPagination(PageNumberPagination):
+class StandardResultsSetPagination(CustomPageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
-    max_page_size = 1000
+    max_page_size = 1000000
 
 
-class SmallResultsSetPagination(PageNumberPagination):
+class SmallResultsSetPagination(CustomPageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 1000000
