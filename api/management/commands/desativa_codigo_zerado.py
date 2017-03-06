66184d411a714f776bd4360c6f679dedad882708
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-03-06 09:23:22 -0300

    Alterando desativa codigo zerados

--- api/management/commands/desativa_codigo_zerado.py
+++ api/management/commands/desativa_codigo_zerado.py
@@ -10,7 +10,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         apresentacoes = Apresentacao.objects.filter(
-            Q(codigo_barras__isnull=True) | Q(codigo_barras=0)
+            Q(codigo_barras__isnull=True) | Q(codigo_barras=0),
+            ativo=True,
         )
         print("{} apresentações...".format(apresentacoes.count()))
         for apresentacao in apresentacoes:
