fcde53edd1ed3b6dccf42cde5e882199a2e2da93
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-07-26 15:54:00 -0300

    URL de logout

--- api/urls.py
+++ api/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     url(r'^login_facebook/$', LoginFacebook.as_view(), name='login-facebook'),
     url(r'^login/cliente/$', Login.as_view(), name='login'),
     url(r'^logout/$', Logout.as_view(), name='logout'),
+    url(r'^auth/logout/$', Logout.as_view(), name='logout'),
     url(r'^auth/users/$', CreateUser.as_view(), name='create-user'),
     url(r'^auth/login/$', LoginDefault.as_view(), name='login-user'),
     url(r'^auth/farmacia/login/$', LoginFarmacia.as_view(), name='login-farmacia'),
