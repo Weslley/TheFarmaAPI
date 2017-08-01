521a03fc7903763cee01a3ddc5d54bff97cb2d99
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-08-01 19:00:34 -0300

    Calculando rendimentos da farmacia

--- api/views/financeiro/base.py
+++ api/views/financeiro/base.py
@@ -2,8 +2,11 @@ from rest_framework import generics
 from rest_framework.response import Response
 import calendar
 import locale
+from datetime import date
+from django.db.models import Sum, F
 
 from api.models.enums.status_conta_receber import StatusContaReceber
+from api.models.enums.status_pedido import StatusPedido
 from api.models.pedido import Pedido
 from api.models.conta_receber_farmacia import ContaReceberFarmacia
 from api.mixins.base import IsAuthenticatedRepresentanteMixin
@@ -21,7 +24,7 @@ class ResumoFinanceiro(generics.GenericAPIView, IsAuthenticatedRepresentanteMixi
         # Filtrando as ultimas 4 vendas
         pedidos = Pedido.objects.filter(
             itens__farmacia__representantes=representante
-        ).order_by('-id')[:4]
+        ).distinct('id').order_by('-id')[:4]
 
         # Filtrando as 4 ultimas contas recebidas
         contas_recebidas = ContaReceberFarmacia.objects.filter(
@@ -39,7 +42,15 @@ class ResumoFinanceiro(generics.GenericAPIView, IsAuthenticatedRepresentanteMixi
         values = []
         for mes in range(1, 13):
             valor = 0.0
-            valor += mes
+            query = Pedido.objects.exclude(
+                status=StatusPedido.CANCELADO_PELA_FARMACIA
+            ).exclude(
+                status=StatusPedido.CANCELADO_PELO_CLIENTE
+            ).filter(
+                log__data_criacao__month=mes,
+                log__data_criacao__year=date.today().year
+            ).aggregate(total=Sum('contas_receber_farmacia__valor_bruto'))
+            valor = query['total'] if query['total'] else 0
             values.append(valor)
 
         data = {
