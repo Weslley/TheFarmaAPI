2ed7593cf315f5026d5ca3ea24f6304a21d203c4
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-08-02 14:55:51 -0300

    Alterando o enum de status do Pedido/Proposta

--- api/models/enums/status_pedido.py
+++ api/models/enums/status_pedido.py
@@ -5,8 +5,12 @@ class StatusPedido(IntEnum):
     """
     Enum para status do Pedido
     """
-    ABERTO = 0
-    ENVIADO = 1
-    ENTREGUE = 2
-    CANCELADO_PELA_FARMACIA = 3
-    CANCELADO_PELO_CLIENTE = 4
+    ABERTO = 0                        # Aguardando propostas
+    ACEITO = 1                        # Proposta selecionada foi aceita
+    AGUARDANDO_ENVIO_FARMACIA = 2     # Caso a farmacia for enviar
+    AGUARDANDO_RETIRADA_CLIENTE = 3   # Caso o cliente for retirar o produto em casa
+    ENVIADO = 4                       # Quando o mesmo for enviado pela farmacia
+    ENTREGUE = 5                      # Quando for feito a confirmação de entrega
+    CANCELADO_PELA_FARMACIA = 6       # Cancelamento pela farmacia - FEITO SOMENTE PELOS ADMINISTRADORES
+    CANCELADO_PELO_CLIENTE = 7        # Quando o cliente não aceita a proposta
+    SEM_PROPOSTA = 8                  # Caso na tenha sido encontrado uma proposta para o cliente
