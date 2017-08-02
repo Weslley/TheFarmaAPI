2c6d164be4b1b50906af60174b627f78d80fd5ff
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-08-02 16:34:47 -0300

    Mandando valores de dinheiro formatado

--- api/serializers/conta_receber_farmacia.py
+++ api/serializers/conta_receber_farmacia.py
@@ -1,10 +1,21 @@
+import locale
 from rest_framework import serializers
 
 from api.models.conta_receber_farmacia import ContaReceberFarmacia
 
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
 
 class ContaReceberFarmaciaSerializer(serializers.ModelSerializer):
+    valor_bruto = serializers.SerializerMethodField()
+    valor_liquido = serializers.SerializerMethodField()
+
     class Meta:
         model = ContaReceberFarmacia
         fields = ('id', 'data_credito', 'data_vencimento', 'valor_bruto', 'valor_liquido', 'status')
 
+    def get_valor_bruto(self, obj):
+        return locale.currency(obj.valor_bruto, grouping=True, symbol=None)
+
+    def get_valor_liquido(self, obj):
+        return locale.currency(obj.valor_liquido, grouping=True, symbol=None)
--- api/serializers/pedido.py
+++ api/serializers/pedido.py
@@ -1,12 +1,22 @@
+import locale
 from rest_framework import serializers
 
 from api.models.pedido import Pedido
 
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
 
 class PedidoSimplesSerializer(serializers.ModelSerializer):
     data_criacao = serializers.DateTimeField(read_only=True, source='log.data_criacao')
+    valor_bruto = serializers.SerializerMethodField()
+    valor_liquido = serializers.SerializerMethodField()
 
     class Meta:
         model = Pedido
         fields = ('id', 'data_criacao', 'valor_bruto', 'valor_liquido', 'status')
 
+    def get_valor_bruto(self, obj):
+        return locale.currency(obj.valor_bruto, grouping=True, symbol=None)
+
+    def get_valor_liquido(self, obj):
+        return locale.currency(obj.valor_liquido, grouping=True, symbol=None)
