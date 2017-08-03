a98886b39fad7a48e49c6bbdba67eaaaffc43712
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-08-03 14:11:01 -0300

    Adicionando invoke

---
+++ tasks.py
@@ -0,0 +1,25 @@
+from invoke import task
+
+
+@task
+def build(ctx):
+    ctx.run('./manage.py makemigrations')
+    ctx.run('./manage.py migrate')
+
+
+@task
+def run(ctx):
+    ctx.run('./manage.py runserver')
+
+@task
+def requirements(ctx):
+    ctx.run("pip freeze | sed 's/@[a-z0-9]\+//' > requirements.txt")
+
+
+@task
+def deploy(ctx):
+    ctx.run("git pull origin master")
+    ctx.run("pip install -r requirements.txt")
+    ctx.run('./manage.py migrate')
+    ctx.run('./manage.py collectstatic --no-input')
+    ctx.run("sudo supervisorctl restart api-thefarma")
