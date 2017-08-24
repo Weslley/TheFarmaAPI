af88dce91dd2721a2cf675d38bd4427b1460a9b2
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-08-24 05:01:24 -0300

    update tasks

--- tasks.py
+++ tasks.py
@@ -17,7 +17,6 @@ def requirements(ctx):
 @task
 def deploy(ctx):
     ctx.run("git pull origin master")
-    ctx.run("pip install -r requirements.txt")
     ctx.run('./manage.py migrate')
     ctx.run('./manage.py collectstatic --no-input')
     ctx.run("sudo supervisorctl restart api-thefarma")
