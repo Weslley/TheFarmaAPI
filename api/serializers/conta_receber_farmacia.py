886f72566f72ffd6e66016d4f734d06cceb663a9
caiovictormc <caiovictor31@live.com>
2018-09-20 08:51:49 -0300

    Removendo e Criando novo serializer para contas a receber

--- api/serializers/conta_receber_farmacia.py
+++ api/serializers/conta_receber_farmacia.py
@@ -4,19 +4,11 @@ from rest_framework import serializers
 
 from api.models.conta_pagar import ContaPagar
 
+
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 
-class ContaReceberFarmaciaSerializer(serializers.ModelSerializer):
-    valor_bruto = serializers.SerializerMethodField()
-    valor_liquido = serializers.SerializerMethodField()
 
-    class Meta:
-        model = ContaPagar
-        fields = ('id', 'data_credito', 'data_vencimento', 'valor_bruto', 'valor_liquido', 'status')
-
-    def get_valor_bruto(self, obj):
-        return locale.currency(obj.valor_bruto, grouping=True, symbol=None)
-
-    def get_valor_liquido(self, obj):
-        return locale.currency(obj.valor_liquido, grouping=True, symbol=None)
+class AnnotationContaReceberSerializer(serializers.Serializer):
+    data_vencimento = serializers.DateTimeField()
+    valor_liquido = serializers.CharField()
