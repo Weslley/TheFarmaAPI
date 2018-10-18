001aca93d64ce6a711a8a56aeaadc10d4a5d3adb
caiovictormc <caiovictormc@gmail.com>
2018-10-18 09:31:43 -0300

    Update redis urls in settings

--- thefarmaapi/settings.py
+++ thefarmaapi/settings.py
@@ -195,8 +195,9 @@ LOGOUT_URL = '/admin/logout/'
 LOGIN_REDIRECT_URL = '/admin/'
 
 # Celery
-BROKER_URL = config('BROKER_URL_INI')
-CELERY_ACCEPT_CONTENT = ['json']
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
