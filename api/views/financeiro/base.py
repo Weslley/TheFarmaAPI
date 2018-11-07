2224436c70677fb2d7ce4208f8ed0c01c5f952cb
caiovictormc <caiovictormc@gmail.com>
2018-11-07 09:41:24 -0300

    Criando view de listagem simplificada para pagina de vendas farmacia

--- api/views/financeiro/base.py
+++ api/views/financeiro/base.py
@@ -18,6 +18,51 @@ from datetime import datetime
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 
+class ResumoListagemVendas(generics.GenericAPIView, IsAuthenticatedRepresentanteMixin):
+    """
+    Resumo da listagem de vendas
+    * Implementacoes de filtros
+    """
+    def get(self, request, *args, **kwargs):
+        representante = self.get_object()
+
+        hoje = datetime.now()
+        pedidos_de_hoje = Pedido.objects\
+            .exclude(
+                Q(status=StatusPedido.CANCELADO_PELA_FARMACIA) | 
+                Q(status=StatusPedido.CANCELADO_PELO_CLIENTE)
+            )\
+            .filter(
+                farmacia__representantes=representante,
+                data_criacao__year=hoje.year,
+                data_criacao__month=hoje.month,
+                data_criacao__day=hoje.day
+            )
+
+        valores_pedidos =  pedidos_de_hoje.aggregate(
+            bruto=Sum('valor_bruto'),
+            liquido=Sum('valor_liquido')
+        )
+
+        for k, v in valores_pedidos.items():
+            if v == None:
+                valores_pedidos[k] = 0
+
+        data = {
+            'resumo': {
+                'valor_bruto': valores_pedidos.get('bruto'),
+                'valor_liquido': valores_pedidos.get('liquido'),
+                'quantidade': pedidos_de_hoje.count()
+            },
+            'data': pedidos_de_hoje
+        }
+        return Response(data)
+
+    def get_object(self):
+        self.check_object_permissions(self.request, self.request.user.representante_farmacia)
+        return self.request.user.representante_farmacia
+
+
 class ResumoFinanceiro(generics.GenericAPIView, IsAuthenticatedRepresentanteMixin):
 
     def get(self, request, *args, **kwargs):
