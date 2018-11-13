6b47b2bc65fd2293dae99772680aa066728e4c09
caiovictormc <caiovictormc@gmail.com>
2018-11-13 11:19:52 -0300

    Criando serializer ContaSerializer

--- api/serializers/conta.py
+++ api/serializers/conta.py
@@ -2,11 +2,20 @@ import locale
 
 from rest_framework import serializers
 
+from api.models.conta import Conta
 from api.models.conta_pagar import ContaPagar
 
 
-class ContaSerializer(serializers.Serializer):
+class ContaMinimalSerializer(serializers.Serializer):
     data_vencimento = serializers.DateTimeField()
     valor_total = serializers.CharField()
     status = serializers.IntegerField()
     tipo = serializers.IntegerField()
+
+
+class ContaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conta
+        fields = (
+            'data_vencimento', 'valor_total', 'status', 'tipo', 'id'
+        )
