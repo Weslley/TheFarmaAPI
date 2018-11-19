abcd8bf055633d472bd0a218bc86d76aa34b1b1d
caiovictormc <caiovictormc@gmail.com>
2018-11-19 11:07:11 -0300

    Criando model boleto

--- api/models/conta.py
+++ api/models/conta.py
@@ -6,33 +6,41 @@
 
 from django.db import models
 
-from api.models.enums import StatusConta, StatusPagamentoConta
+from api.models.boleto import Boleto
 from api.models.farmacia import Farmacia
+from api.models.enums import StatusConta, StatusPagamentoConta
 
 
 class Conta(models.Model):
-	"""
-	Conta em relacao ao TheFarma, tipo PAGAR significa
-	TheFarma pagar a farmacia
-	"""
-	data_criacao = models.DateTimeField(auto_now_add=True)
-	data_atualizacao = models.DateTimeField(auto_now=True)
-	farmacia = models.ForeignKey(Farmacia, related_name='contas')
+    """
+    Conta em relacao ao TheFarma, tipo PAGAR significa
+    TheFarma pagar a farmacia
+    """
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+    farmacia = models.ForeignKey(Farmacia, related_name='contas')
 
-	status = models.IntegerField(
-		choices=StatusPagamentoConta.choices(),
-		default=StatusPagamentoConta.ABERTA
-	)
+    data_emissao = models.DateField(null=True, blank=True)
+    boleto = models.OneToOneField(
+        Boleto, on_delete=models.SET_NULL,
+        related_name='conta',
+        null=True, blank=True
+    )
 
-	tipo = models.IntegerField(
-		choices=StatusConta.choices(),
-		default=StatusConta.PAGAR
-	)
+    status = models.IntegerField(
+        choices=StatusPagamentoConta.choices(),
+        default=StatusPagamentoConta.ABERTA
+    )
 
-	data_vencimento = models.DateField(null=True, blank=True)
-	valor_total = models.DecimalField(
+    tipo = models.IntegerField(
+        choices=StatusConta.choices(),
+        default=StatusConta.PAGAR
+    )
+
+    data_vencimento = models.DateField(null=True, blank=True)
+    valor_total = models.DecimalField(
         max_digits=15, decimal_places=2, default=0
     )
 
-	def __str__(self):
-		return "{}".format(self.farmacia.razao_social)
+    def __str__(self):
+        return "{}".format(self.farmacia.razao_social)
