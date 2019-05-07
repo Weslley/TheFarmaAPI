c7aa11ef2b44dd4b84d7d265d151b1638fb2cd02
mandala21 <lucasresone@gmail.com>
2019-05-07 14:06:35 -0300

    novo metodo de fazer proposta automatica para usuario teste

--- api/utils/usuario_teste.py
+++ api/utils/usuario_teste.py
@@ -1,4 +1,6 @@
 from api.management.commands.faker import Command as FakeCommand
+import random
+from api.models.pedido import ItemPropostaPedido
 
 def check_user_eh_teste(user):
     """
@@ -16,11 +18,32 @@ def fazer_proposta_faker(pedido):
     Farmacia teste faz uma propostas falsa  
     pedido: Pedido  
     """
-    c = FakeCommand()
-    print('ola')
-    c.logar('x@x.com','teste@1234') #loga
-    c.monta_headers() #monta os headers
-    c.fazer_proposta({'id':pedido.id}) #faz a proposta
+    #importa aqui pq treta 
+    # esse modulo chama o modulo das serializer e o serializer chama ele
+    from api.serializers.pedido import PropostaUpdateSerializer
+    #data para salvar
+    data = {
+        'id':pedido.id
+    }
+    itens_proposta = []
+    #recupera os itens pedidos
+    itens = ItemPropostaPedido.objects.filter(pedido=pedido)
+    for item in itens:
+        #faz uma proposta aleatoria
+        itens_proposta.append({
+            'valor_unitario':random.randint(1,50),
+            'quantidade':random.randint(1,item.quantidade),
+            'possui':True,
+            'id':item.id
+        })
+    #adiciona a lista de propostas
+    data.update({
+        'itens_proposta':itens_proposta    
+    })
+    #instancia o serializer e salva
+    serializer = PropostaUpdateSerializer(pedido,data,partial=True)
+    serializer.is_valid()
+    serializer.save()
 
 
 def criar_proposta_faramcia_teste(pedido):
