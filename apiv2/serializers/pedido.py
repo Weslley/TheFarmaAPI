cf2e94da4b1c68cc0797d9d96692449ee4f16744
mandala21 <lucasresone@gmail.com>
2019-07-09 17:26:23 -0300

    inicio da geracao dos itens pedido

--- apiv2/serializers/pedido.py
+++ apiv2/serializers/pedido.py
@@ -7,6 +7,7 @@ from api.models.apresentacao import Apresentacao
 from apiv2.utils.custom_fields import ListPrimaryKeyRelatoionField
 from api.utils import get_client_browser, get_client_ip 
 from api.models.log import Log
+from api.utils import get_user_lookup, get_tempo_proposta
 
 
 
@@ -39,7 +40,8 @@ class PedidoCreateSerializer(serializers.ModelSerializer):
     
     def create(self,validated_data):
         with transaction.atomic():
-            #request da requisicao
+            #init vars
+            itens = validated_data.pop('itens')
             request = self.context['request']
             #cria o log
             # gerando log do pedido com o agent e o ip da requisição
@@ -50,11 +52,14 @@ class PedidoCreateSerializer(serializers.ModelSerializer):
             #atualiza as informacoes do endereco
             endereco = validated_data.pop('endereco',None)
             if validated_data['delivery'] and endereco:
-                 validated_data.update(**self.recupera_dados_endereco(endereco))   
+                validated_data.update(**self.recupera_dados_endereco(endereco))   
+            
             #cria o pedido
+            validated_data['cliente'] = get_user_lookup(request,'cliente')
+            validated_data['log'] = log
             pedido = Pedido.objects.create(**validated_data)
+            
             #cria o pedido dos itens
-            itens = validated_data.pop('itens')
             self.gerar_itens_pedido(itens,pedido)
     
     def recupera_dados_endereco(self,endereco):
@@ -75,7 +80,7 @@ class PedidoCreateSerializer(serializers.ModelSerializer):
             "nome_destinatario": getattr(endereco, 'nome_destinatario', None),
         }
     
-    def gerar_itens_pedido(self,pedido,itens):
+    def gerar_itens_pedido(self,itens,pedido):
         """
         Gera os itens pedido de um pedido
         pedido: Pedido
@@ -83,4 +88,17 @@ class PedidoCreateSerializer(serializers.ModelSerializer):
         return List<ItemPedido>
         """
         for item in itens:
-            import pdb; pdb.set_trace()
+            for apresentacao in item['apresentacao']:
+                data_item_pedido = {}
+                valor_unitario = 0
+                apresentacao.get_manager.update_ranking_proposta(apresentacao.id)
+                # Buscando o pmc base para calcular o valor unitário
+                try:
+                    tabela = apresentacao.tabelas.get(icms=cidade.uf.icms)
+                    valor_unitario = tabela.pmc
+                except Exception as err:
+                    print(err)
+
+                item_data['pedido'] = pedido
+                item_data['valor_unitario'] = valor_unitario
+                ItemPedido.objects.create(**item_data)
