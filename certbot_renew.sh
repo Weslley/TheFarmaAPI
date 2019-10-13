cf1f3108d6fbeb02483a31da45f22abc5112a2db
Weslley de Sousa <weslleyocara@hotmail.com>
2019-10-13 15:34:46 -0300

    Renew CertBot

--- certbot_renew.sh
+++ certbot_renew.sh
@@ -2,3 +2,4 @@
 
 /usr/local/bin/docker-compose -f /root/apps/thefarmaapi/docker-compose.yml run certbot renew --dry-run \
 && /usr/local/bin/docker-compose -f /root/apps/thefarmaapi/docker-compose.yml kill -s SIGHUP nginx
+
